19eaa1ba256bc1952d42620383c6ede420256c43
Vitor <vitorkhaled47@gmail.com>
2024-06-24 20:21:37 -0300

    Adicionar arquivos de projeto.

---
+++ ApiScp/Model/Scp.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiScp.Model
+{
+    public class Scp
+    {
+        public string ItemNumber { get; set; }
+
+        public string ObjectClass { get; set; }
+
+        public string ContainmentProcedures { get; set; }
+
+        public string Description { get; set; }
+    }
+}

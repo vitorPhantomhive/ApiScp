29c266ccf235d1c4ec5ed2827b45c6649d0cf66a
Vitor <vitorkhaled47@gmail.com>
2024-06-28 01:11:22 -0300

    fix: removendo de local para poder usar a todo momento

--- ApiScp/Program.cs
+++ ApiScp/Program.cs
@@ -25,11 +25,10 @@ namespace ApiScp
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
